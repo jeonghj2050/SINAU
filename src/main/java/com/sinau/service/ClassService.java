9f4efd03790a4696f5f4c146b2dd892f6b9d51f0
sangmin-yu <68494036+sangmin-yu@users.noreply.github.com>
2020-08-13 15:08:43 +0900

    commit

--- src/main/java/com/sinau/service/ClassService.java
+++ src/main/java/com/sinau/service/ClassService.java
@@ -588,8 +588,7 @@ public class ClassService {
 			return fMap; 
 
 		}
-	public LikesDto updatedisLikes(String onc_code,  String l_cts_code) {
-		log.info("updatedisLikes()"+onc_code+l_cts_code);
+
 	public LikesDto updatedisLikes(String onc_code, String l_cts_code) {
 		log.info("updatedisLikes()" + onc_code + l_cts_code);
 
