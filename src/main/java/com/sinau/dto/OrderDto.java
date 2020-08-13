726122b7a9ad2770dc5e1fd4812f8c1f7b0a4929
sangmin-yu <68494036+sangmin-yu@users.noreply.github.com>
2020-08-13 17:02:41 +0900

    commit123

--- src/main/java/com/sinau/dto/OrderDto.java
+++ src/main/java/com/sinau/dto/OrderDto.java
@@ -9,14 +9,13 @@ public class OrderDto {
 	private String ord_code;
 	private String ord_name;
 	private String ord_addr;
-	private int ord_phone;
+	private String ord_phone;
 	private String ord_memo;
 	private int ord_price;
 	private String ord_diposit;
 	private Timestamp ord_date;
 	private int ord_state;
 	private String ord_pcode;
-	private int ord_kind;
 	private int ord_amount;
 	private String ord_m_email;
 	
